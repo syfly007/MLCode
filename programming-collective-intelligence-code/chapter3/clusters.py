6c268616dc882b4abf589f64db16135922a222c9
syfly007 <syfly007@163.com>
2017-01-06 07:04:47 +0800

    add

--- programming-collective-intelligence-code/chapter3/clusters.py
+++ programming-collective-intelligence-code/chapter3/clusters.py
@@ -278,3 +278,71 @@ def draw2d(data,labels,jpeg='mds2d.jpg'):
     y=(data[i][1]+0.5)*1000
     draw.text((x,y),labels[i],(0,0,0))
   img.save(jpeg,'JPEG')  
+
+def scaledown3d(data,distance=pearson,rate=0.01):
+  n=len(data)
+
+  # The real distances between every pair of items
+  realdist=[[distance(data[i],data[j]) for j in range(n)] 
+             for i in range(0,n)]
+
+  # Randomly initialize the starting points of the locations in 3D
+  loc=[[random.random(),random.random(),random.random()] for i in range(n)]
+  fakedist=[[0.0 for j in range(n)] for i in range(n)]
+  
+  lasterror=None
+  for m in range(0,1000):
+    # Find projected distances
+    for i in range(n):
+      for j in range(n):
+        fakedist[i][j]=sqrt(sum([pow(loc[i][x]-loc[j][x],2) 
+                                 for x in range(len(loc[i]))]))
+  
+    # Move points
+    grad=[[0.0,0.0,0.0] for i in range(n)]
+    
+    totalerror=0
+    for k in range(n):
+      for j in range(n):
+        if j==k: continue
+        # The error is percent difference between the distances
+        errorterm=(fakedist[j][k]-realdist[j][k])/realdist[j][k]
+        
+        # Each point needs to be moved away from or towards the other
+        # point in proportion to how much error it has
+        grad[k][0]+=((loc[k][0]-loc[j][0])/fakedist[j][k])*errorterm
+        grad[k][1]+=((loc[k][1]-loc[j][1])/fakedist[j][k])*errorterm
+        grad[k][2]+=((loc[k][2]-loc[j][2])/fakedist[j][k])*errorterm
+
+        # Keep track of the total error
+        totalerror+=abs(errorterm)
+    print totalerror
+
+    # If the answer got worse by moving the points, we are done
+    if lasterror and lasterror<totalerror: break
+    lasterror=totalerror
+    
+    # Move each of the points by the learning rate times the gradient
+    for k in range(n):
+      loc[k][0]-=rate*grad[k][0]
+      loc[k][1]-=rate*grad[k][1]
+      loc[k][2]-=rate*grad[k][2]
+
+  return loc
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d
+
+def draw3d(data,labels,jpeg='mds3d.jpg'):
+  x = [data[i][0] for i in range(len(data))]
+  y = [data[i][1] for i in range(len(data))]
+  z = [data[i][2] for i in range(len(data))]
+  ax=plt.subplot(111,projection='3d')
+  ax.scatter(x,y,z,c='y') 
+  ax.set_xlabel('x')
+  ax.set_ylabel('y')
+  ax.set_zlabel('z')
+
+  plt.show()
